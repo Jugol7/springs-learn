a6fb1bb5cc4ad3c07c4afa16b47e963c9cdcbdd6
zlp <971894709@qq.com>
2022-11-28 23:54:25 +0800

    重复读取输入流

--- src/main/java/com/imooc/spring/escape/http_request_response/RequestWrapper.java
+++ src/main/java/com/imooc/spring/escape/http_request_response/RequestWrapper.java
@@ -10,9 +10,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 
+/**
+ * 请求包装器过滤器
+ * 1. Request 中的 getInputStream 和 getReader 都只能用一次（读取一次）
+ * 2. Request 中的 getInputStream 和 getReader 和 getParameter 是互斥的，调用其中一个，再使用另外的方法是获取不到数据的，直接返回了
+ * Response 也是一样的
+ * 解析第一点：
+ * getInputStream()：源码分析是由于在ServletInputStream中，读取数据的数据坐标指针是没有重置的，读完之后，坐标指针一直都是最后，所以读取不到数据
+ * getReader()：也是一样的原理
+ * 解析第二点：
+ * 存在两个boolean变量，usingReader、usingInputStream，在调用了对应方法之后，然后置为 true
+ * 同样可以实现一个 ResponseWrapper
+ *
+ * @author zlp
+ * @date 2022/11/28
+ */
 public class RequestWrapper extends HttpServletRequestWrapper {
 
-    /** 存储输入流数据 */
+    /**
+     * 存储输入流数据
+     */
     private final byte[] body;
 
     /**
@@ -23,7 +40,7 @@ public class RequestWrapper extends HttpServletRequestWrapper {
      */
     public RequestWrapper(HttpServletRequest request) {
         super(request);
-
+        // 保存数据流中的数据
         body = RequestParseUtil.getBodyString(request).getBytes(
                 Charset.defaultCharset());
     }
@@ -33,6 +50,12 @@ public class RequestWrapper extends HttpServletRequestWrapper {
         return new BufferedReader(new InputStreamReader(getInputStream()));
     }
 
+    /**
+     * 可多次调用使用请求数据流中的信息
+     *
+     * @return
+     * @throws IOException
+     */
     @Override
     public ServletInputStream getInputStream() throws IOException {
 
--- src/main/java/com/imooc/spring/escape/http_request_response/RequestWrapperFilter.java
+++ src/main/java/com/imooc/spring/escape/http_request_response/RequestWrapperFilter.java
@@ -2,16 +2,17 @@ package com.imooc.spring.escape.http_request_response;
 
 import lombok.extern.slf4j.Slf4j;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
+/**
+ * 请求包装器过滤器
+ *
+ * @author zlp
+ * @date 2022/11/28
+ */
 @Slf4j
 @WebFilter(urlPatterns = "/*", filterName = "RequestWrapperFilter")
 public class RequestWrapperFilter implements Filter {
@@ -24,7 +25,7 @@ public class RequestWrapperFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response,
                          FilterChain chain) throws IOException, ServletException {
         log.info("RequestWrapperFilter Replace InputStream!");
-
+        // 过滤器处理每一个请求，将Request换成自定义的，可重复读取
         ServletRequest requestWrapper =
                 new RequestWrapper((HttpServletRequest) request);
         chain.doFilter(requestWrapper, response);
