57eead21c5f19832f59ca3cbd88d2daeab8f34bd
zlp <971894709@qq.com>
2022-11-29 23:11:05 +0800

    定时任务

--- src/main/java/com/imooc/spring/escape/scheduled_task/ScheduledTask.java
+++ src/main/java/com/imooc/spring/escape/scheduled_task/ScheduledTask.java
@@ -4,6 +4,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+/**
+ * @Scheduled 中的常见参数
+ * fixedDelay：上次任务结束与下次任务开始的间隔时间（毫秒）
+ * fixedRate：两次任务开始的间隔时间，不管上次任务是否执行完成
+ * initialDelay：一般与上面两个组合使用，表示回等待设置的时间之后才开始第一次执行（第一次执行的延迟时间）
+ * cron：上次任务结束与下次任务开始间隔时间（秒）
+ *
+ * @author zlp
+ * @date 2022/11/29
+ */
 @Slf4j
 @Component
 @SuppressWarnings("all")
